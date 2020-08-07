0029b1a4da21d04cd5dea8fc9b51b23b9bfe3ea8
Michael Porotkin <34237389+preload737773@users.noreply.github.com>
2020-08-07 16:15:34 +0400

    change homepage string

---
+++ build/precache-manifest.b7f463f17c2715f6c37be1c991a32f98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3712d7855ce004b2897b2c2d277593dd",
+    "url": "/intown/build/index.html"
+  },
+  {
+    "revision": "8da53848049f5d7b1ec1",
+    "url": "/intown/build/static/css/2.1e562f28.chunk.css"
+  },
+  {
+    "revision": "8da53848049f5d7b1ec1",
+    "url": "/intown/build/static/js/2.87f2e808.chunk.js"
+  },
+  {
+    "revision": "c2ca3f14a1d34a8f5fe5085412d4ca71",
+    "url": "/intown/build/static/js/2.87f2e808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba19c204b6dcd3c46ae3",
+    "url": "/intown/build/static/js/main.b32e7a4e.chunk.js"
+  },
+  {
+    "revision": "214dcf8489b5af0d999c",
+    "url": "/intown/build/static/js/runtime-main.b449d5b4.js"
+  }
+]);
